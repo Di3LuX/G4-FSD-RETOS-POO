60e7f24a7fa6076790b27be8c0147915714423e5
JesúsVCh <jvalenzuela.chulia@gmail.com>
2022-10-07 13:35:02 +0200

    ejer1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log("Ejercicio01")
+
+class Persona {
+    constructor(dni) {
+        const sexo = "H";
+        this.nombre = "";
+        this.edad = 0;
+        this.sexo = sexo;
+        this.peso = 0;
+        this.altura = 0;
+        this.dni = dni;
+    }
+}
+
+let persona1 = new Persona ("53759792Y");
+console.log (persona1);
+
+let persona2 = new Persona ("5432353Y");
+console.log (persona2);
+
+let persona3 = new Persona ("72392590Y");
+console.log (persona3)
+
+// Ejercicio 2
+console.log("Ejercicio02")
+
